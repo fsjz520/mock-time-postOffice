f8728e11d1e547b52361df481daa7c4698a135ef
fsjz520 <92107255+fsjz520@users.noreply.github.com>
2022-02-12 23:19:53 +0800

    Update default.js

--- server/config/default.js
+++ server/config/default.js
@@ -3,13 +3,13 @@
 module.exports = {
 	port: 3000,  // 端口号
 	mongodbUrl: 'mongodb://localhost/zs',  // 数据库地址
-	domainName: 'http://email.zsjustn.top', // 域名, 配置全局
+	domainName: 'https://gtlingdro.cn', // 域名, 配置全局
 	/* 
 		邮箱地址, 采用的是qq邮箱, 如果需要配置自己的邮箱,
 		需要登录qq邮箱进入设置, 开启POP3/SMTP服务. 具体可百度
 	 */
-	email: '1772591173@qq.com', //邮箱地址 
-	authCode: 'eyebtxkwfciyjgfd',  // 授权码, qq邮箱配置获取
-	emails: ['1772591173@qq.com'], // 配置多个邮箱地址
-	authCodes: ['eyebtxkwfciyjgfd']  // 对应配置多个授权码
+	email: 'q.tringdron@foxmail.com', //邮箱地址 
+	authCode: 'alojdwxlisuabdci',  // 授权码, qq邮箱配置获取
+	//emails: ['1772591173@qq.com'], // 配置多个邮箱地址
+	//authCodes: ['eyebtxkwfciyjgfd']  // 对应配置多个授权码
 }
